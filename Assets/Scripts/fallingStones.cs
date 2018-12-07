715ad0175208b5bfa7820c401799789ba4ee6c89
Reem eslam <reemeslam13@Reems-MacBook-Pro.local>
2018-12-07 13:48:06 +0200

    Add RockObstacle

---
+++ Assets/Scripts/fallingStones.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingStones : MonoBehaviour
+{
+    public GameObject g;
+    public Transform t;
+    int randTime;
+    GameObject rock;
+    // Use this for initialization
+    void Start()
+    {
+        randTime = (int)Random.Range((int)Time.time + 1, (int)Time.time + 20);
+        Debug.Log(randTime);
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randTime == (int)Time.time)
+        {
+            createRockObst();
+        }
+
+    }
+   
+
+    void  createRockObst(){
+        rock = Instantiate(g) as GameObject;
+        rock.tag = "Rock";
+        rock.transform.position = new Vector3(t.position.x, t.position.y + 20, this.t.position.z);
+        randTime = (int)Random.Range((int)Time.time + 5, (int)Time.time + 25);
+        Debug.Log(randTime);
+
+    }
+}
---
+++ Assets/Scripts/kratos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kratos : MonoBehaviour
+{
+    int t;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.GetComponent<Animator>().GetBool("HitByRock")&&(int)Time.time==t){
+            gameObject.GetComponent<Animator>().SetBool("HitByRock", false);
+
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {  
+        if (collision.gameObject.CompareTag("Rock"))
+        {
+           
+            Destroy(collision.gameObject);
+            gameObject.GetComponent<Animator>().SetBool("HitByRock", true);
+            t = (int)(Time.time + 1);
+
+
+        }
+    }
+   
+}
