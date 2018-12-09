922854892b1dd74673375e425b7ba042df82b994
MohamedKhaledAbdelhamid <mohamed.mohamed-ali@student.guc.edu.eg>
2018-12-09 19:29:08 +0200

    you can't go wrong with Initial Commit

---
+++ Assets/Scripts/MoveArrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArrow : MonoBehaviour
+{
+
+    public float speed;
+    float distanceTravelled = 0;
+    Vector3 lastPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += Vector3.Distance(transform.position, lastPosition);
+        lastPosition = transform.position;
+        if (speed != 0)
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }
+		if(distanceTravelled > 30)
+		{
+			Destroy(this.gameObject);
+		}
+    }
+}
