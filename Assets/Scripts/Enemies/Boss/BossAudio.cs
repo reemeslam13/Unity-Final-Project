949cbbb3e701bb78efe58a533a67bf04d84297f6
Asser Hamad <asserhamad96@gmail.com>
2018-12-13 16:27:51 +0200

    Added SFX

---
+++ Assets/Scripts/Enemies/Boss/BossAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAudio : MonoBehaviour {
+
+	public AudioClip bossStart;
+	public AudioClip bossStun;
+	public AudioClip bossDeath;
+	
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AudioSource>().PlayOneShot(bossStart);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void playStun(){
+		
+	}
+}
